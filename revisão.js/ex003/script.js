714b0ca01d6af1d7236177830ae1f84d0fa2d3df
henry4i20 <henrimito1305@gmail.com>
2024-09-24 15:23:58 -0300

    receba

--- revisão.js/ex003/script.js
+++ revisão.js/ex003/script.js
@@ -1,18 +1,19 @@
-let l1 = document.getElementById("l1")
-let l2 = document.getElementById("l2")
-let l3 = document.getElementById("l3")
-let l4 = document.getElementById("l4")
-let l5 = document.getElementById("l5")
-let l6 = document.getElementById("l6")
-let l7 = document.getElementById("l7")
-let l8 = document.getElementById("l8")
-let l9 = document.getElementById("l9")
+var jogador ="X"
+var position=['','','','','','','','','']
 
-var matriz =[[l1, l2, l3],
-            [l4, l5, l6],
-            [l7, l8, l9]]
-function add(){ 
-    document.getElementById("teste").innerHTML = matriz
+function jogar(quadrado, index){
 
+    if(quadrado.innerHTML==""){
+        quadrado.innerHTML = jogador
+        position[index]=jogador
+        if(jogador=='X'){
+            jogador="O"
+        }else{
+            jogador="X"
+        }
+    }
 
 }
+
+
+
