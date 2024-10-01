ca652b74701cb39d4d2fd79234fc77634db3f0c8
Vitrolo <viniciusrabelodominguesdasilva@gmail.com>
2024-10-01 17:22:16 -0300

    receba

--- aula26-09_nodeJs/index.js
+++ aula26-09_nodeJs/index.js
@@ -1,11 +1,23 @@
 const express =require('express')
-
+let personList=[]
 const app=express();
 const port=3000;
+nome="HeNrY"
+app.use(express.json())
 
-app.get('/',(req, res)=>{
-    res.send("receba")
+app.get('/view',(req, res)=>{
+    res.send(personList)
 })
+app.delete('/del/:id', (req,res)=>{
+    const {id}= req.params
+    personList.splice(id,1)
+    
+})
+ app.post('/cadastro',(req,res)=>{
+     const {name,age}=req.body
+    personList.push({name,age})
+     res.send(`recebido! nome user ${name}`)
+ })
 
 app.listen(port, ()=>{
     console.log(`example app listening on port ${port}`)
