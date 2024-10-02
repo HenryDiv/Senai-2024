59cc82b7ff18cef564af680a57bde0d4601a8a87
Vitrolo <viniciusrabelodominguesdasilva@gmail.com>
2024-10-02 16:44:55 -0300

    receba

--- aula26-09_nodeJs/index.js
+++ aula26-09_nodeJs/index.js
@@ -1,19 +1,33 @@
 const express =require('express')
-let personList=[]
+var personList=[]
 const app=express();
 const port=3000;
-nome="HeNrY"
 app.use(express.json())
 
-app.get('/view',(req, res)=>{
+app.get('/view',(req, res)=>{ //view user
     res.send(personList)
 })
-app.delete('/del/:id', (req,res)=>{
+app.delete('/del/:id', (req,res)=>{ //delete user
     const {id}= req.params
-    personList.splice(id,1)
+    const index = parseInt(id, 10);
+    personList.splice(index,1)
+    res.send(`user removido. users restantes: ${JSON.stringify(personList)}`);
+    
     
 })
- app.post('/cadastro',(req,res)=>{
+
+app.put ('/up/:id', (req,res)=>{ //alter user 
+    const {id}=req.params
+    const {name,age}=req.body
+    try{
+        personList[id]={name,age}
+        res.send(`usuario atualizado!${id}\n Novo nome${name}\n nova idade ${age}`)
+    }
+    catch(err){
+        res.send("User not found ")
+    }
+})
+ app.post('/cadastro',(req,res)=>{ //add new user
      const {name,age}=req.body
     personList.push({name,age})
      res.send(`recebido! nome user ${name}`)
